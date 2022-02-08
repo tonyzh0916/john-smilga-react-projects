49e33db3a6c263640c68b54cb67cd87ae59dd104
Yao Zhang <Yao.Zhang@gartner.com>
2022-02-08 17:29:02 -0500

    almost finish project 11 and useref

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,9 +1,11 @@
+//https://react-projects.netlify.app/
 import React from 'react'
 import Navbar from './Navbar'
+
 function App() {
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar/>
     </>
   )
 }
--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,45 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  return (
+  <nav>
+    <div className='nav-cneter'>
+      <div className='nav-header'>
+        <img src={logo} alt="logo"/>
+        <button className='nav-toggle'>
+          <FaBars/>
+        </button>
+      </div>
+      <div className='links-container' ref={linksContainerRef}>
+        <ul className='links' ref={linksRef}>
+          {links.map((link)=>{
+            const {id,url, text}=link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <ul className='social-icons'>
+       {
+         social.map((socialIcon)=>{
+           const {id,url,icon}=socialIcon;
+           return (
+             <li key={id}>
+               <a href={url}>{icon}</a>
+             </li>
+           )
+         })
+       }
+      </ul>
+    </div>
+  </nav>
+    )
 }
 
 export default Navbar
