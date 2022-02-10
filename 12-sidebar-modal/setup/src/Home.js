00a3161d138b375e41d90f699de0f1a34b087530
Yao Zhang <Yao.Zhang@gartner.com>
2022-02-10 11:34:37 -0500

    finished project 12

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,14 +1,19 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className='sidebar-toggle'>
-        <FaBars/>
+      <button onClick={openSidebar} className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button onClick={openModal} className='btn'>
+        show modal
       </button>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
